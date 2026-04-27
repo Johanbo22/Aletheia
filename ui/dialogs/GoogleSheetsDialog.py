3a35f4cb819536eb8933e7f46ae3574960049a87
Johanbo22 <jbk@famkjaer.dk>
2026-04-27 16:13:46 +0200

    tweaks for google sheets dialog

--- ui/dialogs/GoogleSheetsDialog.py
+++ ui/dialogs/GoogleSheetsDialog.py
@@ -1,12 +1,13 @@
 import re
 
 from PyQt6.QtCore import QSettings, Qt
-from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QDialog, QFormLayout, QHBoxLayout, QLabel, QMessageBox, QVBoxLayout, QScrollArea, QWidget
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QDialog, QFormLayout, QHBoxLayout, QLabel, QMessageBox, QVBoxLayout, QWidget, QTabWidget
 
 from ui.widgets import DataPlotStudioButton, DataPlotStudioGroupBox, DataPlotStudioLineEdit, DataPlotStudioComboBox
 from ui.theme import ThemeColors
-
+from ui.icons import IconBuilder, IconType
+from core.resource_loader import get_resource_path
 
 class GoogleSheetsDialog(QDialog):
     """Dialog for importing data from Google Sheets"""
@@ -14,9 +15,10 @@ class GoogleSheetsDialog(QDialog):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setWindowTitle("Import from Google Sheets")
+        self.setWindowIcon(QIcon(get_resource_path("icons/menu_bar/google-sheets-logo-icon.svg")))
         self.setWindowFlag(Qt.WindowType.WindowContextHelpButtonHint, False)
         self.setModal(True)
-        self.resize(650, 650)
+        self.resize(650, 400)
         self.setMinimumWidth(500)
         self.gid = None
 
@@ -25,30 +27,36 @@ class GoogleSheetsDialog(QDialog):
     def init_ui(self) -> None:
         """Initialize dialog UI"""
         main_layout = QVBoxLayout()
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        main_layout.setSpacing(0)
+        main_layout.setContentsMargins(10, 10, 10, 10)
+        main_layout.setSpacing(10)
         
-        scroll_area = QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        scroll_area.setFrameShape(QScrollArea.Shape.NoFrame)
-        scroll_area.setProperty("styleClass", "transparent_scroll_area")
+        self.tab_widget = QTabWidget()
+        connection_tab = QWidget()
+        connection_layout_main = QVBoxLayout(connection_tab)
+        connection_layout_main.setContentsMargins(20, 20, 20, 20)
+        connection_layout_main.setSpacing(15)
         
-        scroll_content = QWidget()
-        scroll_content.setObjectName("TransparentScrollContent")
-        content_layout = QVBoxLayout(scroll_content)
-        content_layout.setContentsMargins(20, 20, 20, 20)
-        content_layout.setSpacing(15)
-
-        # Information label
-        info_label = QLabel("Enter your Google Sheets details below:")
+        header_layout = QHBoxLayout()
+        # Info label
+        info_label = QLabel("Enter your Google Sheets details below")
         info_label.setObjectName("google_sheets_info_label")
-        content_layout.addWidget(info_label)
-
+        header_layout.addWidget(info_label)
+        
+        header_layout.addStretch()
+        
+        self.help_button = DataPlotStudioButton("How to Import?", parent=self)
+        self.help_button.setToolTip("View instructions for importing Google Sheets data")
+        self.help_button.setIcon(IconBuilder.build(IconType.Information))
+        self.help_button.clicked.connect(self.show_instructions)
+        header_layout.addWidget(self.help_button)
+        
+        connection_layout_main.addLayout(header_layout)
+        
         # Form layout for inputs
         connection_group = DataPlotStudioGroupBox("Connection Details", parent=self)
-        connection_layout = QFormLayout()
-
-        # Sheet ID input
+        connection_form_layout = QFormLayout()
+        
+        # Shee ID
         sheet_id_label = QLabel("Google Sheet Link or Sheet ID:")
         self.sheet_id = DataPlotStudioComboBox()
         self.sheet_id.setEditable(True)
@@ -57,29 +65,50 @@ class GoogleSheetsDialog(QDialog):
         self.sheet_id.lineEdit().setClearButtonEnabled(True)
         self.sheet_id.setMinimumWidth(350)
         self.sheet_id.editTextChanged.connect(self.parse_input)
-        connection_layout.addRow(sheet_id_label, self.sheet_id)
-
-        # Sheet Name input
+        
+        sheet_id_layout = QHBoxLayout()
+        sheet_id_layout.setContentsMargins(0, 0, 0, 0)
+        sheet_id_layout.addWidget(self.sheet_id)
+        
+        self.status_icon = QLabel()
+        self.status_icon.setFixedSize(20, 20)
+        self.status_icon.setScaledContents(True)
+        self.status_icon.setToolTip("Valid Google Sheet ID detected")
+        self.status_icon.hide()
+        sheet_id_layout.addWidget(self.status_icon)
+        
+        self.sheet_id.editTextChanged.connect(self.parse_input)
+        connection_form_layout.addRow(sheet_id_label, sheet_id_layout)
+        
+        # Sheet Name
         sheet_name_label = QLabel("Sheet Name:")
         self.sheet_name = DataPlotStudioLineEdit()
-        self.sheet_name.setToolTip("This is the name of the sheet you want to import data from.")
+        self.sheet_name.setToolTip("The name of the sheet you want to import data from")
         self.sheet_name.setPlaceholderText("e.g., Sheet1")
         self.sheet_name.setClearButtonEnabled(True)
-        connection_layout.addRow(sheet_name_label, self.sheet_name)
+        connection_form_layout.addRow(sheet_name_label, self.sheet_name)
+        
+        connection_group.setLayout(connection_form_layout)
+        connection_layout_main.addWidget(connection_group)
+        connection_layout_main.addStretch()
+        
+        self.tab_widget.addTab(connection_tab, QIcon(IconBuilder.build(IconType.Connect)), "Connection")
+        
+        # Advanced Settings Tab
+        advanced_tab = QWidget()
+        advanced_layout_main = QVBoxLayout(advanced_tab)
+        advanced_layout_main.setContentsMargins(20, 20, 20, 20)
+        advanced_layout_main.setSpacing(15)
 
-        connection_group.setLayout(connection_layout)
-        content_layout.addWidget(connection_group)
-
-        #delimter
         delimiter_group = DataPlotStudioGroupBox("CSV Delimiter Settings", parent=self)
         delimiter_layout = QVBoxLayout()
 
-        delimiter_info = QLabel("Google Sheets exports data as a CSV. Choose the delimiter used in your sheet")
+        delimiter_info = QLabel("Google Sheets exports data as a CSV. Choose the delimiter used in your region.")
         delimiter_info.setWordWrap(True)
         delimiter_info.setProperty("styleClass", "muted_text")
         delimiter_layout.addWidget(delimiter_info)
 
-        #delimter box
+        # Delimiter settings box
         delimiter_form_layout = QFormLayout()
 
         self.delimiter_combo = DataPlotStudioComboBox()
@@ -93,21 +122,21 @@ class GoogleSheetsDialog(QDialog):
         ])
         self.delimiter_combo.setCurrentIndex(0)
         self.delimiter_combo.currentTextChanged.connect(self.on_delimiter_changed)
-        delimiter_form_layout.addRow("Delimiter:",self.delimiter_combo)
+        delimiter_form_layout.addRow("Delimiter:", self.delimiter_combo)
 
-        #custom
+        # Custom delimiter
         self.custom_delimiter_input = DataPlotStudioLineEdit()
-        self.custom_delimiter_input.setPlaceholderText("Enter single delimiter character")
+        self.custom_delimiter_input.setPlaceholderText("Enter single character")
         self.custom_delimiter_input.setMaxLength(1)
         self.custom_delimiter_input.setEnabled(False)
-        self.custom_delimiter_input.setMaximumWidth(100)
+        self.custom_delimiter_input.setMaximumWidth(150)
         
         custom_delimiter_hbox = QHBoxLayout()
         custom_delimiter_hbox.addWidget(self.custom_delimiter_input)
         custom_delimiter_hbox.addStretch()
         delimiter_form_layout.addRow("Custom Delimiter:", custom_delimiter_hbox)
 
-        #decimal sep
+        # Decimal separator
         self.decimal_combo = DataPlotStudioComboBox()
         self.decimal_combo.addItems([
             "Dot (.) - UK/US",
@@ -116,7 +145,7 @@ class GoogleSheetsDialog(QDialog):
         self.decimal_combo.setCurrentIndex(0)
         delimiter_form_layout.addRow("Decimal Separator:", self.decimal_combo)
 
-        #1000sep
+        # Thousands separator
         self.thousands_combo = DataPlotStudioComboBox()
         self.thousands_combo.addItems([
             "None",
@@ -129,38 +158,19 @@ class GoogleSheetsDialog(QDialog):
 
         delimiter_layout.addLayout(delimiter_form_layout)
         delimiter_group.setLayout(delimiter_layout)
-        content_layout.addWidget(delimiter_group)
+        advanced_layout_main.addWidget(delimiter_group)
+        advanced_layout_main.addStretch()
 
-        # Help text
-        help_text = QLabel(
-            "<b>How to use:</b><br><br>"
-            "1. Open your Google Sheet in a browser.<br>"
-            "2. Copy the ID from the URL:<br>"
-            "&nbsp;&nbsp;&nbsp;<span style='color: #555555;'>docs.google.com/spreadsheets/d/<b>[SHEET_ID]</b>/edit</span><br>"
-            "3. Check the sheet tab name (bottom left corner).<br>"
-            "4. <b>IMPORTANT:</b> Share the sheet publicly<br>"
-            "&nbsp;&nbsp;&nbsp;<i>(File → Share → \"Anyone with the link\")</i>.<br>"
-            "5. Select appropriate delimiter and decimal for your region.<br>"
-            "6. Paste the ID and sheet name above."
-        )
-        help_text.setTextFormat(Qt.TextFormat.RichText)
-        help_text.setFont(QFont("Arial", 9))
-        help_text.setProperty("styleClass", "blue_help_box")
-        help_text.setWordWrap(True)
-        content_layout.addWidget(help_text)
+        self.tab_widget.addTab(advanced_tab, QIcon(IconBuilder.build(IconType.Settings)), "Advanced Settings")
         
-        content_layout.addStretch()
+        main_layout.addWidget(self.tab_widget)
         
-        scroll_area.setWidget(scroll_content)
-        main_layout.addWidget(scroll_area)
-
-        # Button layout
         button_container = QWidget()
         button_layout = QHBoxLayout(button_container)
-        button_layout.setContentsMargins(20, 10, 20, 20)
+        button_layout.setContentsMargins(10, 5, 10, 10)
         button_layout.addStretch()
 
-        self.import_button = DataPlotStudioButton("Import", base_color_hex=ThemeColors.MainColor, parent=self)
+        self.import_button = DataPlotStudioButton("Import", base_color_hex=ThemeColors.MainColor, text_color_hex="white", parent=self)
         self.import_button.setDefault(True)
         self.import_button.setMinimumWidth(100)
         self.import_button.setEnabled(False)
@@ -177,6 +187,24 @@ class GoogleSheetsDialog(QDialog):
 
         self.load_history()
         self.sheet_id.setFocus()
+    
+    def show_instructions(self) -> None:
+        help_text = (
+            "<b>How to use Google Sheets Import:</b><br><br>"
+            "1. Open your Google Sheet in a browser.<br>"
+            "2. Copy the ID from the URL:<br>"
+            "&nbsp;&nbsp;&nbsp;<span style='color: #888888;'>docs.google.com/spreadsheets/d/<b>[SHEET_ID]</b>/edit</span><br>"
+            "3. Check the sheet tab name (bottom left corner).<br>"
+            "4. <b>IMPORTANT:</b> Share the sheet publicly<br>"
+            "&nbsp;&nbsp;&nbsp;<i>(File → Share → \"Anyone with the link\")</i>.<br>"
+            "5. If needed, configure delimiters in the <b>Advanced Settings</b> tab.<br>"
+            "6. Paste the ID and sheet name into the connection dialog."
+        )
+        msg_box = QMessageBox(self)
+        msg_box.setWindowTitle("Google Sheets Import Instructions")
+        msg_box.setTextFormat(Qt.TextFormat.RichText)
+        msg_box.setText(help_text)
+        msg_box.exec()
 
     def on_delimiter_changed(self, text) -> None:
         """Handle delimiter selection change"""
@@ -190,8 +218,10 @@ class GoogleSheetsDialog(QDialog):
         self.import_button.setEnabled(bool(text.strip()))
         # Regex to match sheet id
         id_match = re.search(r"/d/([a-zA-Z0-9-_]+)", text)
+        is_valid_id = False
 
         if id_match:
+            is_valid_id = True
             extracted_id = id_match.group(1)
             if self.sheet_id.currentText() != extracted_id:
                 self.sheet_id.blockSignals(True)
@@ -215,6 +245,15 @@ class GoogleSheetsDialog(QDialog):
                 self.gid = None
                 self.sheet_name.setEnabled(True)
                 self.sheet_name.setPlaceholderText("e.g., Sheet1")
+                if len(text.strip()) > 20 and re.match(r"^[a-zA-Z0-9-_]+$", text.strip()):
+                    is_valid_id = True
+        
+        if is_valid_id:
+            self.status_icon.setPixmap(QIcon(IconBuilder.build(IconType.Checkmark)).pixmap(20, 20))
+            self.status_icon.show()
+        else:
+            self.status_icon.hide()
+        
 
     def validate_and_accept(self) -> None:
         """Validate inputs before accepting"""
